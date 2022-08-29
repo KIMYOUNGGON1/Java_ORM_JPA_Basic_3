187bc8cebcc1691677f9836ec51a0edc1ddb7db9
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-08-29 10:50:57 +0900

    2022-08-29 자바 ORM 표준 JPA 프로그래밍 - 기본편 (Named 쿼리)

--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -1,8 +1,13 @@
 package jpql;
 
 import javax.persistence.*;
+import javax.xml.namespace.QName;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
