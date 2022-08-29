0839e99d87764f0a28f704720eb637c87738aa20
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-08-29 10:23:56 +0900

    2022-08-29 자바 ORM 표준 JPA 프로그래밍 - 기본편 (패치 조인2 - 한계)

--- src/main/java/jpql/Team.java
+++ src/main/java/jpql/Team.java
@@ -1,5 +1,7 @@
 package jpql;
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,8 +17,10 @@ public class Team {
 
     private String name;
 
+    @BatchSize(size = 100)
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
+
 
     public Long getId() {
         return id;
